1fd7810ab46e64e4d2bf8aafb1acd398802b85cb
mick <mjdorma@gmail.com>
2013-08-04 15:30:00 +1000

    improve demo to show changes in input params and configuration object

--- docs/demo.py
+++ docs/demo.py
@@ -1,28 +1,33 @@
 import begin
 import funconf
 
-
 config = funconf.Config('my.conf')
 
-
 @begin.subcommand
-@config.foo.wraps
+@config.foo
 def foo(**k):
     """This is the foo code"""
-    print(k)
-
+    print("Foo got %s" % k)
+    print("Config is:")
+    print(config)
 
 @begin.subcommand
-@config.bread.wraps
+@config.bread
 def bread(**k):
     """This is the bread code"""
-    print(k)
+    print("Bread got %s" % k)
+    print("Config is:")
+    print(config)
 
+@begin.subcommand
+@config
+def run(**k):
+    """This is the run command that controls all"""
+    print("Run got %s" % k)
+    print("Config is:")
+    print(config)
 
 @begin.start
-@config.wraps
-def main(**k):
-    """teeeehiii"""
-    print(k)
-
+def entry():
+    pass
 
